0960141e06dce7cf7ae90164bfbe559ac78c5adb
Olof Bjarnason <olof.bjarnason@gmail.com>
2019-07-26 22:24:25 +0200

    wip approval tests and code

--- src/djur.py
+++ src/djur.py
@@ -1,1 +1,49 @@
-print("hej")
+# coding: utf-8
+
+
+def djur(db, _input=None, _print=None):
+    _inp = _input or input
+
+    def inp():
+        print(">>> ", end='')
+        ans = _inp().lower()
+        return ans[0] if len(ans) > 0 else ' '
+
+    prn = _print or print
+    prn("Välkommen till GISSA DJUR!")
+    prn("--------------------------")
+    prn("Jag känner till 2 djur.")
+    while True:
+        prn("(S)pela eller (A)vsluta?")
+        ans = inp()
+        if ans == 'a':
+            prn("Tack för att du spelade!")
+            return
+        elif ans == 's':
+            prn("Tänk på ett djur, så ska jag gissa vilket du tänker på!")
+            prn("När du tänkt klart, skriv (K)lart.")
+            while inp() != 'k':
+                prn("Va? Skriv k när du är klar!")
+            prn("OK då kör vi...")
+            pos = db
+            # Questions are 4 length tuples!
+            while len(pos) == 4:
+                prn(f"{pos[0]} - (J)a eller (N)ej?")
+                ans = inp()
+                if ans not in "jn":
+                    prn("Jag förstår bara j och n, svenska alltså!")
+                    continue
+                if ans == 'j':
+                    pos = pos[2]
+                else:
+                    pos = pos[3]
+            djur = pos
+            prn(f"Jag gissar att du tänkte på {djur}!")
+            prn("Hade jag rätt? (J)a eller (N)ej?")
+            ans = inp()
+            if ans == 'j':
+                prn("Vad kul! :D :D :D")
+        else:
+            prn(f"Jag förstår inte '{ans}'!")
+
+
--- tests/test_djur.py
+++ tests/test_djur.py
@@ -1,2 +1,99 @@
-def test_1():
-	assert True
+# coding: utf-8
+from pprint import pprint
+
+from src.djur import djur
+
+
+def test_approval_test_game_correct_guess():
+    got = []
+
+    db = (
+        'Kan djuret simma', 'j',
+        ('gädda'),
+        ('örn')
+    )
+
+    fake_answers = ['spela', 'q', 'K', 'y', 'j', 'j', 'a']
+    def fake_input():
+        next_answer = fake_answers.pop(0)
+        got.append(f"INP {next_answer}")
+        return next_answer
+
+    def fake_print(msg):
+        got.append(f"PRN {msg}")
+
+    # print, input
+    djur(db, _input=fake_input, _print=fake_print)
+    pprint(got)
+    expected = [
+        "PRN Välkommen till GISSA DJUR!",
+        "PRN --------------------------",
+        "PRN Jag känner till 2 djur.",
+        "PRN (S)pela eller (A)vsluta?",
+        "INP spela",
+        "PRN Tänk på ett djur, så ska jag gissa vilket du tänker på!",
+        "PRN När du tänkt klart, skriv (K)lart.",
+        "INP q",
+        "PRN Va? Skriv k när du är klar!",
+        "INP K",
+        "PRN OK då kör vi...",
+        "PRN Kan djuret simma - (J)a eller (N)ej?",
+        "INP y",
+        "PRN Jag förstår bara j och n, svenska alltså!",
+        "PRN Kan djuret simma - (J)a eller (N)ej?",
+        "INP j",
+        "PRN Jag gissar att du tänkte på gädda!",
+        "PRN Hade jag rätt? (J)a eller (N)ej?",
+        "INP j",
+        "PRN Vad kul! :D :D :D",
+        "PRN (S)pela eller (A)vsluta?",
+        "INP a",
+        "PRN Tack för att du spelade!",
+    ]
+    assert expected == got
+
+
+def test_approval_test_no_actual_game():
+    got = []
+
+    fake_answers = ['q', '', 'a']
+    def fake_input():
+        next_answer = fake_answers.pop(0)
+        got.append(f"INP {next_answer}")
+        return next_answer
+
+    def fake_print(msg):
+        got.append(f"PRN {msg}")
+
+    # print, input
+    djur(None, _input=fake_input, _print=fake_print)
+    pprint(got)
+    expected = [
+        "PRN Välkommen till GISSA DJUR!",
+        "PRN --------------------------",
+        "PRN Jag känner till 2 djur.",
+        "PRN (S)pela eller (A)vsluta?",
+        "INP q",
+        "PRN Jag förstår inte 'q'!",
+        "PRN (S)pela eller (A)vsluta?",
+        "INP ",
+        "PRN Jag förstår inte ' '!",
+        "PRN (S)pela eller (A)vsluta?",
+        "INP a",
+        "PRN Tack för att du spelade!"
+    ]
+    assert expected == got
+
+
+if __name__ == '__main__':
+    db = (
+        'Kan djuret simma', 'j',
+        ('gädda'),
+        ('örn')
+    )
+    djur(db)
+
+
+# önskvärda features
+# djurformattering exvis "GÄDDA " --> "gädda"
+# frågeformattering exvis "kan den flyga?" -> "Kan den flyga"
